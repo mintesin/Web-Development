555de10d7bf92944390dc0a5464597633c28fcbb
mintesin <50635173+mintesin@users.noreply.github.com>
2022-10-06 16:51:44 +0530

    Update main.py

--- Blog(with Jinja2/main.py
+++ Blog(with Jinja2/main.py
@@ -1,11 +1,9 @@
 from flask import Flask,render_template,url_for
 from data import data
-import datetime
+
 import time
 import jinja2
-import requests
-# url='https://www.npoint.io/docs/4a937eb8edf3a8d9dedb'
-# response=requests.get(url)
+
 json_data=data
 # print(data)
 #
